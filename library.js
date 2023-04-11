c3702bf9e2e864cfac47c79f9e689f2b8c1b0c9d
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-11 17:41:13 +0300

    test rendering to canvas

--- library.js
+++ library.js
@@ -1,33 +1,38 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.151.3/three.module.js';
 //not remember but maybe it need be like a module or something like this
 
-var scenes_cameras_renderers = {};
+export var scenes_cameras_renderers = {};
+export var objects_materials = {};
+export var animates_handlers = {};
+
 //api
-function create_3D_button(canvas_id,caller,width,height,rotation_x,rotation_y,rotation_z,color,text) {
+export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotation_y,rotation_z,color,text) {
     console.log("create_3D_button\n");
     prepare_WebGL_context(canvas_id);
+    
+    objects_material[canvas_id] = {}
 
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    objects_material[canvas_id].object = new THREE.Mesh( geometry, material );
+    scene.add( objects_material[canvas_id].object );
 
     scenes_cameras_renderers[canvas_id].camera.position.z = 5;
 
-    function animate() {
-	    requestAnimationFrame( animate );
+    animates_handlers[canvas_id].animate = function () {
+	    requestAnimationFrame( animates_handlers[canvas_id].animate );
 
-	    cube.rotation.x += 0.01;
-	    cube.rotation.y += 0.01;
+	    objects_material[canvas_id].object.rotation.x += 0.01;
+	    objects_material[canvas_id].object.rotation.y += 0.01;
 
 	    renderer.render( scenes_cameras_renderers[canvas_id].scene, scenes_cameras_renderers[canvas_id].camera );
     }
 
-    animate();
+    animates_handlers[canvas_id].animate();
 }
 
 //common functions for api's
-function prepare_WebGL_context(canvas_id,library="three.js"){
+export function prepare_WebGL_context(canvas_id,library="three.js"){
     console.log("prepare_WebGL_context\n");
     scenes_cameras_renderers[canvas_id] = {}
     scenes_cameras_renderers[canvas_id].scene    = new THREE.Scene();
