c5d30d6dafb36b1d0446fdc81738588b4cacbfd4
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-11 16:16:03 +0300

    test export

---
+++ library.js
@@ -0,0 +1,15 @@
+//api
+function create_3D_button(canvas_id,caller,width,height,rotation_x,rotation_y,rotation_z,color,text) {
+    console.log("create_3D_button\n");
+    prepare_WebGL_context();
+//grab canvas element
+//prepare context
+//create mesh with properties
+//connect caller to handle
+}
+
+//common functions for api's
+function prepare_WebGL_context(library="three.js"){
+    console.log("prepare_WebGL_context\n");
+}
+
