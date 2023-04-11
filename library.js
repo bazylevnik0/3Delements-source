fd6bcb3e79040d7001f7a6df36e380c1d5490235
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-11 17:44:41 +0300

    test rendering to canvas

--- library.js
+++ library.js
@@ -10,20 +10,20 @@ export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotati
     console.log("create_3D_button\n");
     prepare_WebGL_context(canvas_id);
     
-    objects_material[canvas_id] = {}
-
+    objects_materials[canvas_id] = {};
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    objects_material[canvas_id].object = new THREE.Mesh( geometry, material );
-    scene.add( objects_material[canvas_id].object );
+    objects_materials[canvas_id].object = new THREE.Mesh( geometry, material );
+    scene.add( objects_materials[canvas_id].object );
 
     scenes_cameras_renderers[canvas_id].camera.position.z = 5;
 
+    animates_handlers[canvas_id]=  {};
     animates_handlers[canvas_id].animate = function () {
 	    requestAnimationFrame( animates_handlers[canvas_id].animate );
 
-	    objects_material[canvas_id].object.rotation.x += 0.01;
-	    objects_material[canvas_id].object.rotation.y += 0.01;
+	    objects_materials[canvas_id].object.rotation.x += 0.01;
+	    objects_materials[canvas_id].object.rotation.y += 0.01;
 
 	    renderer.render( scenes_cameras_renderers[canvas_id].scene, scenes_cameras_renderers[canvas_id].camera );
     }
@@ -37,7 +37,6 @@ export function prepare_WebGL_context(canvas_id,library="three.js"){
     scenes_cameras_renderers[canvas_id] = {}
     scenes_cameras_renderers[canvas_id].scene    = new THREE.Scene();
     scenes_cameras_renderers[canvas_id].camera   = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    scenes_cameras_renderers[canvas_id].renderer = new THREE.WebGLRenderer();
-    
+    scenes_cameras_renderers[canvas_id].renderer = new THREE.WebGLRenderer();   
 }
 
