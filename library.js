beb0297db76366d8bbafdd6509f5c8ccc08faf7e
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-13 15:25:39 +0300

    change data structure

--- library.js
+++ library.js
@@ -3,78 +3,53 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 			
-export var scenes_cameras_renderers = {};
-export var objects_materials_models = {};
-export var animates_handlers = {};
+export var data = {};
 
 //api
 export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotation_y,rotation_z,color,text) {
     console.log("create_3D_button in",canvas_id,"\n");
+    data[canvas_id] = {};
     prepare_WebGL_context(canvas_id);
     
-    scenes_cameras_renderers[canvas_id].camera.position.z = 5;
-
-    animates_handlers[canvas_id]=  {};
-    animates_handlers[canvas_id].animate = function () {
-	    requestAnimationFrame( animates_handlers[canvas_id].animate );
-
-	    //objects_materials_models[canvas_id].object.rotation.x += 0.01;
-	    //objects_materials_models[canvas_id].object.rotation.y += 0.01;
-
-	    scenes_cameras_renderers[canvas_id].renderer.render( scenes_cameras_renderers[canvas_id].scene, scenes_cameras_renderers[canvas_id].camera );
+    //set animation loop and handlers
+    data[canvas_id].camera.position.z = 5; //default position of camera
+    data[canvas_id].animate = function () {
+	    requestAnimationFrame( data[canvas_id].animate );
+	    data[canvas_id].renderer.render( data[canvas_id].scene, data[canvas_id].camera );
     }
     
-    
-    
-    objects_materials_models[canvas_id] = {};
-    /*
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    objects_materials_models[canvas_id].object = new THREE.Mesh( geometry, material );
-    scenes_cameras_renderers[canvas_id].scene.add( objects_materials_models[canvas_id].object );
-    */
-    const dracoLoader = new DRACOLoader();
-			dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
+    //load models 
+    let dracoLoader = new DRACOLoader();
+		  dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
 
-			const loader = new GLTFLoader();
-			loader.setDRACOLoader( dracoLoader );
-			loader.load( 'https://bazylevnik0.github.io/3Delements-source/models/button.glb', function ( gltf ) {
-
-				objects_materials_models[canvas_id].model = gltf.scene;
-				objects_materials_models[canvas_id].model.position.set( 1, 1, 0 );
+	let loader = new GLTFLoader();
+		  loader.setDRACOLoader( dracoLoader );
+		  loader.load( 'https://bazylevnik0.github.io/3Delements-source/models/button.glb', function ( gltf ) {
+				data[canvas_id].model = gltf.scene;
+				data[canvas_id].model.position.set( 1, 1, 0 );
 				//model.scale.set( 0.01, 0.01, 0.01 );
-				scenes_cameras_renderers[canvas_id].scene.add( objects_materials_models[canvas_id].model );
-
+				data[canvas_id].scene.add( data[canvas_id].model );
 				//mixer = new THREE.AnimationMixer( model );
 				//mixer.clipAction( gltf.animations[ 0 ] ).play();
-
-				animates_handlers[canvas_id].animate();
-
+				data[canvas_id].animate();
 			}, undefined, function ( e ) {
-
 				console.error( e );
-
 			} );
 
-
-    scenes_cameras_renderers[canvas_id].light = {}
-scenes_cameras_renderers[canvas_id].light.ambientLight = new THREE.AmbientLight()
-scenes_cameras_renderers[canvas_id].light.pointLight = new THREE.PointLight()
-scenes_cameras_renderers[canvas_id].light.pointLight.position.set(10, 10, 10)
-scenes_cameras_renderers[canvas_id].scene.add(scenes_cameras_renderers[canvas_id].light.ambientLight)
-scenes_cameras_renderers[canvas_id].scene.add(scenes_cameras_renderers[canvas_id].light.pointLight)
-
-    
-
-    //animates_handlers[canvas_id].animate();
+    //add light
+    data[canvas_id].ambientLight = new THREE.AmbientLight()
+    data[canvas_id].pointLight = new THREE.PointLight()
+    data[canvas_id].pointLight.position.set(10, 10, 10)
+    data[canvas_id].scene.add(data[canvas_id].ambientLight)
+    data[canvas_id].scene.add(data[canvas_id].pointLight)
 }
 
 //common functions for api's
 export function prepare_WebGL_context(canvas_id,library="three.js"){
-    let canvas = document.getElementById(canvas_id);
     console.log("prepare_WebGL_context for ",canvas_id,"\n");
-    scenes_cameras_renderers[canvas_id] = {}
-    scenes_cameras_renderers[canvas_id].scene    = new THREE.Scene();
-    scenes_cameras_renderers[canvas_id].camera   = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    scenes_cameras_renderers[canvas_id].renderer = new THREE.WebGLRenderer( { canvas: canvas } );
+    data[canvas_id].canvas = document.getElementById(canvas_id);
+    data[canvas_id].scene    = new THREE.Scene();
+                                                                //must be related to canvas size not to window
+    data[canvas_id].camera   = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    data[canvas_id].renderer = new THREE.WebGLRenderer( { canvas: canvas } );
 }
