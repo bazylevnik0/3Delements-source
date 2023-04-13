1849ac592b5592fdb8eb4fecdfe508564e4732c7
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-13 16:30:14 +0300

    test animation

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-//test animation 1
+//test animation 2
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 			
@@ -7,7 +7,8 @@ export var data = {};
 
 //api
 export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotation_y,rotation_z,color,text) {
-    console.log("create_3D_button in",canvas_id,"\n");
+    console.log("create_3D_button in",canvas_id,"...");
+    
     data[canvas_id] = {};
     prepare_WebGL_context(canvas_id);
     
@@ -29,6 +30,7 @@ export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotati
 				data[canvas_id].model.position.set( 1, 1, 0 );
 				data[canvas_id].scene.add( data[canvas_id].model );
 				data[canvas_id].mixer = new THREE.AnimationMixer( data[canvas_id].model );
+				data[canvas_id].animations = gltf.animations;
 				//temp:
 				for (let i = 0; i < gltf.animations.length; i++) {                      
                     data[canvas_id].mixer.clipAction( gltf.animations[ i ] ).play(); 
@@ -45,14 +47,19 @@ export function create_3D_button(canvas_id,caller,width,height,rotation_x,rotati
     data[canvas_id].pointLight.position.set(10, 10, 10)
     data[canvas_id].scene.add(data[canvas_id].ambientLight)
     data[canvas_id].scene.add(data[canvas_id].pointLight)
+    
+    console.log(" ...done\n");
 }
 
 //common functions for api's
 export function prepare_WebGL_context(canvas_id,library="three.js"){
-    console.log("prepare_WebGL_context for ",canvas_id,"\n");
+    console.log("prepare_WebGL_context for ",canvas_id,"...");
+    
     data[canvas_id].canvas = document.getElementById(canvas_id);
     data[canvas_id].scene    = new THREE.Scene();
                                                                 //must be related to canvas size not to window
     data[canvas_id].camera   = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     data[canvas_id].renderer = new THREE.WebGLRenderer( { canvas: data[canvas_id].canvas, alpha: true } );
+    
+    console.log(" ...done\n");
 }
