ab9e75574a0f52b4d194a0b03e31ca30666e1e22
bazylevnik0 <bazylevnik0@gmail.com>
2023-04-13 15:48:10 +0300

    test transparent renderer

--- library.js
+++ library.js
@@ -51,5 +51,5 @@ export function prepare_WebGL_context(canvas_id,library="three.js"){
     data[canvas_id].scene    = new THREE.Scene();
                                                                 //must be related to canvas size not to window
     data[canvas_id].camera   = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    data[canvas_id].renderer = new THREE.WebGLRenderer( { canvas: data[canvas_id].canvas } );
+    data[canvas_id].renderer = new THREE.WebGLRenderer( { canvas: data[canvas_id].canvas, alpha: true } );
 }
