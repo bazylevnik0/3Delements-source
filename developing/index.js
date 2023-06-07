73459e1991cd17019b6f763d2518b22d01389edb
bazylevnik0 <bazylevnik0@gmail.com>
2023-06-07 16:52:56 +0300

    developing: added loader and position,scale configs

--- developing/index.js
+++ developing/index.js
@@ -2,18 +2,30 @@ import * as EL3D from "./library.js"
 
 let viewer0 = {
     canvas_id: "canvas_0",
+    mode: "viewer",
+    width: 0.25,
+    position_x: -1,
 }
 viewer0 = new EL3D.Viewer(viewer0);
 viewer0.init();
 
 let viewer1 = {
     canvas_id: "canvas_1",
+    mode: "viewer",
+    width: 0.25,
+    height: 2.225,
+    position_x: 1,
 }
 viewer1 = new EL3D.Viewer(viewer1);
 viewer1.init();
 
 let viewer2 = {
     canvas_id: "canvas_2",
+    mode: "background",
+    width: 0.5,
+    height: 0.5,
+    depth: 0.5,
+    position_z: 10,
 }
 viewer2 = new EL3D.Viewer(viewer2);
 viewer2.init();
